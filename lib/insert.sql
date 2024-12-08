9959f6b7731823b9ce0ad2db5bc26ac5413db5e6
essie-kamau043 <essie.k004@gmail.com>
2024-12-08 21:26:47 +0300

    Add all changes

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'Brown', 'Sweet', 1),
+('Rowdy', 7, 'M', 'Black', 'Playful', 1),
+('Tabitha', 5, 'F', 'Golden', 'Calm', 1),
+('Sergeant Brown', 12, 'M', 'Brown', 'Serious', 1),
+('Melissa', 6, 'F', 'Black', 'Friendly', 1),
+('Grinch', 4, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 3, 'F', 'White', 'Gentle', 1),
+(NULL, 8, 'F', 'Grey', 'Mysterious', 0);
